12be7452a96cf24662295ed76958f74ce49eb9f1
김부희 <kimbuhui@naver.com>
2021-02-27 04:15:35 +0900

    python web scrapper

--- python-web-scrapper/course4/venv/main.py
+++ python-web-scrapper/course4/venv/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from so import get_jobs 
 
 app = Flask("SuperScrapper")
+
+db = {} # fake db
 
 # @는 바로 아래에 있는 함수만을 찾으며, 함수를 deco 해주는 역할
 @app.route("/")
@@ -11,8 +14,25 @@ def main():
 @app.route("/search")
 def search():
     word = request.args.get('word')
-    #return f"You are looking for a job in {word}"
-    return render_template("result.html",searchingBy=word, test="hi")
+    if word:
+        word = word.lower()  # 소문자로 변환
+        jobs = get_jobs(word)
+        # print(jobs)
+
+        # 4.5 다시 듣기
+        fromDB = db.get(word)
+        if fromDB:
+            jobs = fromDB
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+
+    # rendering 작업
+    return render_template("result.html", searchingBy=word,
+    resultNumber=len(jobs), test="hi")
+    # return f"You are looking for a job in {word}"
 
 
 # @app.route("/<username>")
